4a1c506173861260e9cc2f3355b96c719fe11c71
juan carlos <maciasnajeraj@gmail.com>
2021-08-20 17:10:15 -0500

    added , footer and section about me

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,16 @@
 import {obtn,cbtn} from './responsiveNav.js';
+import {cbtna} from './close.js';
 import loader from './loader.js';
 
+   
     loader
     obtn 
     cbtn
+    cbtna
+    
+    
 
 
-
-
-  
 
 
 
--- assets/js/responsiveNav.js
+++ assets/js/responsiveNav.js
@@ -1,4 +1,4 @@
-let cbtn = document.getElementById("closebtn");
+const cbtn = document.getElementById("closebtn");
 const obtn = document.getElementById("openbtn");
 
 
